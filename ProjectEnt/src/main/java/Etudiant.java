eb888e252b67e86c9cd30cd532e099ef1a8bd7e0
PAIN <anthoninpain@hotmail.fr>
2024-01-11 08:29:54 +0100

    Mise à jour Notes + Lien Etudiants et Utilisateurs

--- ProjectEnt/src/main/java/Etudiant.java
+++ ProjectEnt/src/main/java/Etudiant.java
@@ -1,17 +1,20 @@
-public class Etudiant {
+import java.util.ArrayList;
+
+public class Etudiant extends Utilisateur{
 
     private String nom;
     private String prenom;
     private String spe;
     private int INE;
-    private double[] notes;
+    private ArrayList<Notes> notes;
 
-    public Etudiant(String nom, String prenom, String spe, int INE) {
+    public Etudiant(String nom, String prenom, String spe, int INE, String id, String mdp, String type) {
+        super(id,mdp,type);
         this.nom = nom;
         this.prenom = prenom;
         this.spe = spe;
         this.INE = INE;
-        this.notes = new double[0]; // Initialisation d'un tableau vide de notes
+        
     }
 
     public String getNom() {
@@ -30,26 +33,14 @@ public class Etudiant {
         return this.INE;
     }
 
-    public double[] getNotes() {
+    public ArrayList<Notes> getNotes() {
         return this.notes;
     }
 
     // Méthode pour ajouter une note
-    public void ajouterNote(double nouvelleNote) {
-        // Création d'un nouveau tableau avec une case de plus
-        double[] nouveauTableau = new double[this.notes.length + 1];
-
-        // Copie des anciennes notes dans le nouveau tableau
-        for (int i = 0; i < this.notes.length; i++) {
-            nouveauTableau[i] = this.notes[i];
-        }
-
-        // Ajout de la nouvelle note à la fin du tableau
-        nouveauTableau[this.notes.length] = nouvelleNote;
-
-        // Remplacement du tableau de notes par le nouveau tableau
-        this.notes = nouveauTableau;
+    public void ajouterNote(Notes nouvelleNote) {
+        
+        this.notes.add(nouvelleNote);
     }
-
-    // Autres méthodes si nécessaire
-}
+  }
+        
--- ProjectEnt/src/main/java/Notes.java
+++ ProjectEnt/src/main/java/Notes.java
@@ -1,28 +1,40 @@
 public class Notes {
-	    // Attributs
-	    private double valeur;
-	    private String module;
+        // Attributs
+        private double valeur;
+        private String module;
+        private int semestre;
+        private Etudiant etudiant;
 
-	    // Constructeur
-	    public Notes(double valeur, String module) {
-	        this.valeur = valeur;
-	        this.module = module;
-	    }
+        // Constructeur
+        public Notes(double valeur, String module, int semestre, Etudiant etudiant) {
+            this.valeur = valeur;
+            this.module = module;
+            this.semestre = semestre;
+            this.etudiant=etudiant;
+        }
 
-	    // Méthodes d'accès (getters et setters)
-	    public double getValeur() {
-	        return valeur;
-	    }
+        // Méthodes d'accès (getters et setters)
+        public double getValeur() {
+            return valeur;
+        }
 
-	    public void setValeur(double valeur) {
-	        this.valeur = valeur;
-	    }
+        public void setValeur(double valeur) {
+            this.valeur = valeur;
+        }
 
-	    public String getModule() {
-	        return module;
-	    }
+        public String getModule() {
+            return module;
+        }
 
-	    public void setModule(String module) {
-	        this.module = module;
-	    }
+        public void setModule(String module) {
+            this.module = module;
+        }
+        
+        public int getSemestre() {
+            return this.semestre;
+        }
+        
+        public Etudiant getEtudiant() {
+            return this.etudiant;
+        }
 }
