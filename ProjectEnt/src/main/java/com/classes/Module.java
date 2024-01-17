419e79a7b5f4f3b9251fcd0ab99bf907b42f169b
Anthonin <anthoninpain@hotmail.fr>
2024-01-17 23:47:32 +0100

    ajout de classes matiere et module

---
+++ ProjectEnt/src/main/java/com/classes/Module.java
@@ -0,0 +1,51 @@
+package com.classes;
+
+import java.util.Arrays;
+
+public class Module {
+    private int id;
+    private String nom;
+    private Matiere[] matieres;
+
+    // Constructeur
+    public Module(int id, String nom, Matiere[] matieres) {
+        this.id = id;
+        this.nom = nom;
+        this.matieres = matieres;
+    }
+
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Matiere[] getMatieres() {
+        return matieres;
+    }
+
+    public void setMatieres(Matiere[] matieres) {
+        this.matieres = matieres;
+    }
+
+    // Autres méthodes si nécessaire
+    @Override
+    public String toString() {
+        return "Module{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", matieres=" + Arrays.toString(matieres) +
+                '}';
+    }
+}
