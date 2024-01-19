a5718a30fbd837b28271e7f396db0ab0783c5192
Yelauu <127487673+ThomasVly@users.noreply.github.com>
2024-01-19 19:54:30 +0100

    test

--- ProjectEnt/src/main/java/com/classes/Etudiant.java
+++ ProjectEnt/src/main/java/com/classes/Etudiant.java
@@ -7,13 +7,15 @@ public class Etudiant extends Utilisateur{
     private String prenom;
     private String spe;
     private String INE;
+    private Module module;
     
-    public Etudiant(String nom, String prenom, String spe, String INE, String id, String mdp, String type) {
+    public Etudiant(String nom, String prenom, String spe, String INE, String id, String mdp, String type, Module module) {
         super(id,mdp,type);
         this.nom = nom;
         this.prenom = prenom;
         this.spe = spe;
         this.INE = INE;
+        this.module = module;
         
     }
 
@@ -32,12 +34,15 @@ public class Etudiant extends Utilisateur{
     public String getIne() {
         return this.INE;
     }
+    public Module getModule() {
+    	return this.module;
+    }
 
 
   
 
     public String toString() {
-    	return "nom : "+ this.nom + "prenom : " +this.prenom + "specialite : " + this.spe + "INE : "+ this.INE;   
+    	return "nom : "+ this.nom + "prenom : " +this.prenom + "specialite : " + this.spe + "INE : "+ this.INE + "module : "+ this.module.getNom();   
     			}
 
     // Autres méthodes si nécessaire
